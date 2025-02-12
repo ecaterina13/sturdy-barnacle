88a5881c321b0ccd2f1eb933d2b42397d3f66cf6
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-02-12 12:47:06 -0600

    I nitial commit

---
+++ spec/team_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require './lib/player'
+require'./lib/team'
+RSpec.describe Team do
+    it 'exists' do
+      team = Team.new("Dodgers", "Los Angeles")
+  
+      expect(team).to be_instance_of(Team)
+    end
+    it 'has a roster' do
+        team = Team.new("Dodgers", "Los Angeles")
+    
+        expect(team.roster).to eq([])
+      end
+
+      it 'has a player_count' do
+        team = Team.new("Dodgers", "Los Angeles")
+    
+        expect(team.player_count).to eq(0)
+      end
+
+
+      it 'add_player' do
+        team = Team.new("Dodgers", "Los Angeles")
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        expect(team.roster).to eq([player_1])
+        #expect(team.player_count).to eq(1)
+        
+
+        team.add_player(player_2)
+        expect(team.roster).to eq([player_1, player_2])
+        #expect(team.player_count).to eq(2)
+
+        team.add_player(player_3)
+        expect(team.roster).to eq([player_1, player_2, player_3])
+        #expect(team.player_count).to eq(3)
+
+        team.add_player(player_4)
+        expect(team.roster).to eq([player_1, player_2, player_3, player_4])
+        #expect(team.player_count).to eq(4)
+       
+      end
+
+      it 'team_player_count' do
+        team = Team.new("Dodgers", "Los Angeles")
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+        expect(team.player_count).to eq(2)
+      end
+
+    it 'finds long_term_players' do
+        team = Team.new("Dodgers", "Los Angeles")
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.long_term_players
+
+        expect(team.long_term_players).to eq([player_1, player_3])
+    
+       # expect(team.short_term_players).to eq([player_2, player_4])
+    end
+end
+
+
